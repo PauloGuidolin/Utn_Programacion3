032543ca5af32d93f47f4bd1f14dfb6bd17ea1e8
PauloGuidolin <paulog162003@gmail.com>
2024-09-12 17:01:14 -0300

    js terminado

--- javascript/main.js
+++ javascript/main.js
@@ -75,7 +75,7 @@ esPar(8);
 
 //ejercicio 2
 
-function convertirCelsiusAFahrenheit(celsius){
+function convertirCelsiusAFahrenheit(celsius) {
     let fahrenheit = (celsius * 1.8) + 32;
     console.log(`${celsius} grados celsius son ${fahrenheit} grados fahrenheit`);
 }
@@ -91,32 +91,32 @@ let persona = {
 };
 
 console.log(persona);
-function cambiarCiudad(persona, ciudadNueva){
-    persona.ciudad=ciudadNueva;
+function cambiarCiudad(persona, ciudadNueva) {
+    persona.ciudad = ciudadNueva;
 }
-cambiarCiudad(persona,"Madrid");
+cambiarCiudad(persona, "Madrid");
 console.log(persona);
 
 //ejercicio 2
 
 
-let libro={
+let libro = {
     titulo: "El señor de los anillos",
     autor: "J.R.R. Tolkien",
     anioPublicacion: 2010
 };
 
-function esMayora10(cuento){
-    if((2024-cuento.anioPublicacion)>=10){
+function esMayora10(cuento) {
+    if ((2024 - cuento.anioPublicacion) >= 10) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-if(esMayora10(libro)){
+if (esMayora10(libro)) {
     console.log(`el libro ${libro.titulo} es mayor a 10 años ${esMayora10(libro)}`);
-}else{
+} else {
     console.log(`el libro ${libro.titulo} es mayor a 10 años ${esMayora10(libro)}`);
 }
 
@@ -124,23 +124,23 @@ if(esMayora10(libro)){
 
 //ARRAYS
 //ejercicio 1
-let numeros = [1, 2, 3, 4, 5,6,7,8,9,10];
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(numeros);
-let multiploDos=[]
+let multiploDos = []
 
-for(let i=0;i<numeros.length; i++){
-    let num=numeros[i]*2
-    multiploDos[i]=num
+for (let i = 0; i < numeros.length; i++) {
+    let num = numeros[i] * 2
+    multiploDos[i] = num
 }
 
 console.log(multiploDos);
 
 //ejercicio 2
-let numerosPares=[];
+let numerosPares = [];
 
-for(let i=0;i<20;i++){
-    if(i%2==0){
-        numerosPares[i]=i;
+for (let i = 0; i < 20; i++) {
+    if (i % 2 == 0) {
+        numerosPares[i] = i;
     }
 }
 console.log(numerosPares);
@@ -148,21 +148,77 @@ console.log(numerosPares);
 //INTRODUCCION AL DOM
 //ejercicio 1
 let boton = document.getElementById("boton");
-let contenedor=document.getElementById("parrafos");
+let contenedor = document.getElementById("parrafos");
 
-boton.addEventListener("click", function() {
-contenedor.style.color = "blue";
+boton.addEventListener("click", function () {
+    contenedor.style.color = "blue";
 });
 
 //ejercicio 2
 let texto = document.getElementById("texto");
 let enviar = document.getElementById("enviar");
 
-enviar.addEventListener('click',function(){
+enviar.addEventListener('click', function () {
     alert(texto.value);
 })
-//ejercicio 3
-let pepe=document.createElement("pepe");
-li1.addEventListener("click",function(){
-    console.log("hola");
+
+//EVENTOS EN DOM
+//ejercicio 1
+let element1 = document.getElementById("li1");
+let element2 = document.getElementById("li2");
+let element3 = document.getElementById("li3");
+let element4 = document.getElementById("li4");
+
+function mostrarTexto(elemento) {
+    elemento.addEventListener("click", function () {
+        console.log(elemento.textContent);
+    })
+}
+mostrarTexto(element1);
+mostrarTexto(element2);
+mostrarTexto(element3);
+mostrarTexto(element4);
+
+//ejercicio 2
+
+let text = document.getElementById("text1");
+let habilitar = document.getElementById("hab");
+let deshabilitar = document.getElementById("des");
+
+//funcion para deshabilitar
+deshabilitar.addEventListener("click", function () {
+    text.disabled = true;
 })
+
+//funcion para habilitar
+habilitar.addEventListener('click', function () {
+    text.disabled = false;
+})
+
+//LocalStorage
+let formulario = document.getElementById("formulario");
+let guardar = document.getElementById("submit-correo");
+let correo = document.getElementById("email-guardado");
+let eliminar=document.getElementById("eliminar");
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let mail = document.getElementById("mail");
+
+    localStorage.setItem("userEmail", mail.value);
+    correo.textContent = `Correo Guardado: ${mail.value}`;
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    let email = localStorage.getItem("userEmail");
+    if (email) {
+        correo.textContent = "Correo guardado: " + email;
+    }
+});
+
+//function eliminarCorreo
+
+eliminar.addEventListener("click", function () {
+    localStorage.removeItem("userEmail"); //remueve el correo guardado
+    correo.textContent = ""; 
+});
